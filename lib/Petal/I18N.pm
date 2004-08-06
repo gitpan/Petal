f12012b20649d339706c929ac9a6173039767006
Jean-Michel Hiver <jhiver@cpan.org>
2004-08-06 13:09:30 +0000

    Import of JHIVER/Petal-2.12 from CPAN.
    gitpan-cpan-distribution: Petal
    gitpan-cpan-version:      2.12
    gitpan-cpan-path:         JHIVER/Petal-2.12.tar.gz
    gitpan-cpan-author:       JHIVER
    gitpan-cpan-maturity:     released

--- lib/Petal/I18N.pm
+++ lib/Petal/I18N.pm
@@ -5,16 +5,23 @@ package Petal::I18N;
 use MKDoc::XML::TreeBuilder;
 use MKDoc::XML::TreePrinter;
 use Petal::Hash::String;
-use MKDoc::XML::Decode;
-use strict;
 use warnings;
-use Carp;
+use strict;
+
+our $Namespace = "http://xml.zope.org/namespaces/i18n";
+our $Prefix    = 'i18n';
+our $Domain    = 'default';
 
 
 sub process
 {
     my $class = shift;
     my $data  = shift;
+
+    local $Namespace = $Namespace;
+    local $Prefix    = $Prefix;
+    local $Domain    = $Domain;
+
     my @nodes = MKDoc::XML::TreeBuilder->process_data ($data);
     for (@nodes) { $class->_process ($_) }
     return MKDoc::XML::TreePrinter->process (@nodes);
@@ -27,13 +34,32 @@ sub _process
     my $tree  = shift;
     return unless (ref $tree);
 
+    local $Prefix = $Prefix;
+    local $Domain = $Domain;
+
+    # process the I18N namespace
+    foreach my $key (keys %{$tree})
+    {
+        my $value = $tree->{$key};
+        if ($value eq $Namespace)
+        {
+            next unless ($key =~ /^xmlns\:/);
+            delete $tree->{$key};
+            $Prefix = $key;
+            $Prefix =~ s/^xmlns\://;
+        }
+    }
+
+    # set the current i18n:domain
+    $Domain = delete $tree->{"$Prefix:domain"} || $Domain;
+
     my $tag  = $tree->{_tag};
     my $attr = { map { /^_/ ? () : ( $_ => $tree->{$_} ) } keys %{$tree} };
     return if ($tag eq '~comment' or $tag eq '~pi' or $tag eq '~declaration');
-    
+
     # replace attributes with their respective translations 
-    $tree->{"i18n:attributes"} && do {
-        my $attributes = $tree->{"i18n:attributes"};
+    $tree->{"$Prefix:attributes"} && do {
+        my $attributes = $tree->{"$Prefix:attributes"};
         $attributes =~ s/\s*;\s*$//;
         $attributes =~ s/^\s*//;
         my @attributes = split /\s+\;\s+/, $attributes;
@@ -56,7 +82,7 @@ sub _process
                 $attribute_name = $attribute;
                 $translate_id = _canonicalize ( $tree->{$attribute_name} );
             }
-           
+
             # the default value if maketext() fails should be the current
             # value of the attribute
             my $default_value = $tree->{$attribute_name};
@@ -74,14 +100,14 @@ sub _process
     };
 
     # replace content with its translation
-    exists $tree->{"i18n:translate"} && do {
+    exists $tree->{"$Prefix:translate"} && do {
         my ($translate_id);
 
-        # if we have i18n:translate="something",
+        # if we have $Domain:translate="something",
         # then the translate_id is 'something'
-        if (defined $tree->{"i18n:translate"} and $tree->{"i18n:translate"} ne '')
+        if (defined $tree->{"$Prefix:translate"} and $tree->{"$Prefix:translate"} ne '')
         {
-            $translate_id = $tree->{"i18n:translate"};
+            $translate_id = $tree->{"$Prefix:translate"};
         }
 
         # otherwise, the translate_id has to be computed
@@ -125,11 +151,9 @@ sub _process
 
     # I know, I know, the I18N namespace processing is a bit broken...
     # It should suffice for now.
-    delete $tree->{"xmlns:i18n"};
-    delete $tree->{"i18n:domain"};
-    delete $tree->{"i18n:attributes"};
-    delete $tree->{"i18n:translate"};
-    delete $tree->{"i18n:name"};
+    delete $tree->{"$Prefix:attributes"};
+    delete $tree->{"$Prefix:translate"};
+    delete $tree->{"$Prefix:name"};
 
     # Do the same i18n thing with child nodes, recursively.
     # for some reason it always makes me think of roller coasters.
@@ -165,7 +189,7 @@ sub _extract_named_nodes
     foreach my $node (@nodes)
     {
         $count++;
-        my $name = $node->{"i18n:name"} || $count;
+        my $name = $node->{"$Prefix:name"} || $count;
         $nodes{$name} = $node;
     }
     
@@ -187,7 +211,7 @@ sub _extract_content_string
         };
         
         $count++;
-        my $name = $node->{"i18n:name"} || $count;
+        my $name = $node->{"$Prefix:name"} || $count;
         push @res, '${' . $name . '}';
     }
     
@@ -211,7 +235,17 @@ Petal::I18N - Attempt at implementing ZPT I18N for Petal
 in your Perl code:
 
   use Petal;
-  use Petal::TranslationService::h4x0r;
+  use Petal::TranslationService::Gettext;
+
+  my $translation_service = new Petal::TranslationService::Gettext (
+      locale_dir  => '/path/to/my/app/locale',
+      target_lang => gimme_target_lang(), 
+  );
+
+  my $template = new Petal (
+      file => 'example.html',
+      translation_service => $translation_service
+  );
 
   # we want to internationalize to the h4x0rz 31337 l4nGu4g3z. w00t!
   my $translation_service = Petal::TranslationService::h4x0r->new();
@@ -223,69 +257,150 @@ in your Perl code:
   print $template->process ();
 
 
-in silly_example.xhtml
-
-  <html><body>
-    <!-- this is a mad example of romanized japanese, which we
-         are going to turn into h4x0rz r0m4n|z3d J4paN33z -->
-
-    <div i18n:translate="">
-      Konichiwa, <span i18n:name="name">Buruno</span>-san,
-      Kyoo wa o-genki desu ka?
-    </div>
-  </body></html>
+=head1 I18N Howto
 
 
-... And you get something like:
+=head2 Preparing your templates:
 
-  <html><body>
-    <!-- this is a mad example of romanized japanese, which we
-         are going to turn into h4x0rz r0m4n|z3d J4paN33z -->
+Say your un-internationalized template looks like this:
 
-    <div>K0N1cH1W4, <span>Buruno</span>-s4N, Ky00 w4 o-geNkI DesU kA?</div>
-  </body></html>
+  <html xmlns:tal="http://purl.org/petal/1.0/">
+    <body>
+      <img src="/images/my_logo.png"
+           alt="the logo of our organisation" />
+
+      <p>Hello,
+         <span petal:content="user_name">Joe</span>.</p>
+
+      <p>How are you today?</p>
+    </body>
+  </html>
 
 
-=head1 HOW IT WORKS
+You need to markup your template according to the ZPT I18N specification, which
+can be found at
+http://dev.zope.org/Wikis/DevSite/Projects/ComponentArchitecture/ZPTInternationalizationSupport
 
-You simply instanciate any kind of object and pass it when you construct the
-Petal object, as described in the synopsis. 
+  <html xmlns:tal="http://purl.org/petal/1.0/"
+        xmlns:i18n="http://xml.zope.org/namespaces/i18n"
+        i18n:domain="my_app">
+    <body>
+      <img src="/images/my_logo.png"
+           alt="the logo of our organisation"
+           i18n:attributes="alt" />
+      <p i18n:translate="">Hello, <span petal:content="user_name">Joe</span>.</p>
+      <p i18n:translate="">How are you today?</p>
+    </body>
+  </html>
 
-As long as this object has an instance method called maketext ($stuff), it'll
-work.
 
-At the moment there are two TranslationService objects shipped with the library:
+=head2 Extracting I18N strings:
+
+Once your templates are marked up properly, you will need to use a tool to
+extract the I18N strings into .pot (po template) files. To my knowledge you can
+use i18ndude (standalone python executable), i18nextract.py (part of Zope 3),
+or L<I18NFool>.
+
+I use i18ndude to find strings which are not marked up properly with
+i18n:translate attributes and I18NFool for extracting strings and managing .po
+files.
+
+Assuming you're using i18nfool:
+
+  mkdir -p /path/to/my/app/locale
+  cd /path/to/my/app/locale
+  i18nfool-extract /path/to/my/template/example.html
+  mkdir en
+  mkdir fr
+  mkdir es
+  i18nfool-update
+
+Then you translate the .po files into their respective target languages. When
+that's done, you type:
+
+  cd /path/to/my/app/locale
+  i18nfool-build
+
+And it builds all the .mo files.
+
+
+=head2 Making your application use a Gettext translation service:
+
+Previously you might have had:
+
+  use Petal;
+  # lotsa code here
+  my $template = Petal->new ('example.html');
+
+This needs to become:
+
+  use Petal;
+  use Petal::TranslationService::Gettext;
+  # lotsa code here
+  my $template = Petal->new ('example.html');
+  $template->{translation_service} = Petal::TranslationService::Gettext->new (
+      locale_dir  => '/path/to/my/app/locale',
+      target_lang => gimme_language_code(),
+  );
+
+Where gimme_language_code() returns a language code depending on LC_LANG,
+content-negotiation, config-file, or whatever mechanism you are using to decide
+which language is desired.
+
+
+=head2 And then?
+
+And then that's it! Your application should be easily internationalizable.
+There are a few traps / gotchas thought, which are described below.
+
+
+=head1 BUGS, TRAPS, GOTCHAS and other niceties
+
+
+=head2 Translation Phase
+
+The translation step takes place ONLY ONCE THE TEMPLATE HAS BEEN PROCESSED.
+
+So if you have:
+
+  <p i18n:translate="">Hello,
+    <span i18n:name="user_login" tal:replace="self/user_login">Joe</span>
+  </p>
+
+It most likely will not work because the tal:replace would remove the <span>
+tag and also the i18n:name in the process.
+
+This means that instead of receiving something such as:
+
+  Hello, ${user_login}
+
+The translation service would receive:
+
+  Hello, Fred Flintstone
+
+Or
+
+  Hello, Joe SixPack
+
+etc.
+
+To fix this issue, use tal:content instead of tal:replace and leave the span
+and its i18n:name attribute.
+
+
+=head2 Character sets
+
+I haven't worried too much about them (yet) so if you run into trouble join the
+Petal mailing list and we'll try to fix any issues together.
+
+
+=head2 Limitations
+
+At the moment, L<Petal::I18N> supports the following constructs:
 
 =over 4
 
-=item Petal::TranslationService::h4x0r (rather useless - but kinda fun)
-
-=item Petal::TranslationService::MOFile (works with .mo files produced by gettext)
-
-=back
-
-So if you want to use a .mo file to translate your template, you just do:
-
-   my $ts = Petal::TranslationService::MOFile->new ("/path/to/file.mo");
-
-   my $t  = Petal->new ( file => '/path/to/template.xml',
-                         translation_service => $ts );
-
-   print $t->process (%args);
-
-
-=head1 MORE INFORMATION
-
-You can find the I18N specification at this address.
-
-  L<http://dev.zope.org/Wikis/DevSite/Projects/ComponentArchitecture/ZPTInternationalizationSupport>
-
-At the moment, L<Petal> supports the following constructs:
-
-
-=over 4
-
-=item xmlns:i18n="http://xml.zope.org/namespaces/i18n" - strips it
+=item xmlns:i18n="http://xml.zope.org/namespaces/i18n"
 
 =item i18n:translate
 
@@ -298,17 +413,5 @@ At the moment, L<Petal> supports the following constructs:
 =back
 
 It does *NOT* (well, not yet) support i18n:source, i18n:target or i18n:data.
-Also note that namespace support is not implemented properly: you cannot change
-the prefix, so declaring
-
-  xmlns:internationalization="http://xml.zope.org/namespaces/i18n"
-
-will not work.
-
-
-=head1 I18N HOWTO
-
-... coming soon :-)
 
 =cut
-
